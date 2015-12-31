0375196fcd2286c00aa82c5ca82cef0541e0a1bb
BlackEagle <ike.devolder@gmail.com>
2015-12-31 16:20:35 +0100

    test.sh should not have been committed
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-string='usr/src/linux-4.3.3-GRSRV/tools/perf/arch/common.h'
-#string='usr/src/linux-4.3.3-GRSRV/tools/perf/arch/x86/common.h'
-#string='usr/src/linux-4.3.3-GRSRV/tools/perf/arch/arm/common.h'
-
-if [[ "$string" =~ /arch/.*/ ]] && ! [[ "$string" =~ "/arch/x86/" ]]; then
-    echo 'dont add'
-fi
